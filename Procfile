c4db1610ea252e2fc6ff58187e71dce7ce06f3cb
ReneeSlamka <renee.slamka@gmail.com>
2016-05-08 18:12:24 -0700

    Deployment updates

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/college-kid-budgeter-backend-1.0-SNAPSHOT.jar
--- src/main/java/web/CollegeKidBudgeterApp.java
+++ src/main/java/web/CollegeKidBudgeterApp.java
@@ -2,11 +2,19 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class CollegeKidBudgeterApp {
-    public static void main(String[] args) {
+public class CollegeKidBudgeterApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CollegeKidBudgeterApp.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(CollegeKidBudgeterApp.class, args);
     }
 }
